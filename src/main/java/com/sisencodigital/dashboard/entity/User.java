a0d1633b26cc8f019e069a961afdb49111a2cc9e
CharakaGunathilake-Newnop <cgunathilake@newnop.net>
2026-07-07 19:46:22 +0530

    feat: implement UserDetails fields to User entity for handling security validations

--- src/main/java/com/sisencodigital/dashboard/entity/User.java
+++ src/main/java/com/sisencodigital/dashboard/entity/User.java
@@ -7,6 +7,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
@@ -14,7 +20,7 @@ import lombok.NoArgsConstructor;
 @Table(name = "users")
 @NoArgsConstructor
 @AllArgsConstructor
-public class User extends BaseEntity {
+public class User extends BaseEntity implements UserDetails {
     @Column(nullable = false, length = 100)
     private String name;
     @Column(nullable = false, unique = true, length = 100)
@@ -24,6 +30,32 @@ public class User extends BaseEntity {
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private UserRole role;
+    @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private UserStatus status;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return !status.equals(UserStatus.INACTIVE);
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return !status.equals(UserStatus.BLOCKED);
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
 }
